9a5b6ed334ebe8b15725a08e0bd1b557b85bf4d2
sekimuli <ssekimuliandrew321@gmail.com>
2024-12-24 18:18:54 +0300

    fix bootstrap

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,8 +8,7 @@
     <link
         href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         rel="stylesheet"
-        integrity="sha384-MD7zLAt1HwbQ1sACXl5F7Cnlkmo0rPSr+wpWvMFS18sADfCwBjKufzQH5KxYl/Up"
-        crossorigin="anonymous"
+       
     >
 </head>
 <body class="bg-light">
@@ -19,18 +18,15 @@
 
     <script
         src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-        crossorigin="anonymous"
+        
     ></script>
     <script
         src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"
-        integrity="sha384-ZjEBdEKzc6WaMtuXxVfqJ3VBrU5r9c7UJIcvRhzE8Ub8AJzE1j6XB6cG/vwrxH9u"
-        crossorigin="anonymous"
+        
     ></script>
     <script
         src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha384-B4gt1jrGC7Jh4AgBsrLPwfhDWV8y00d9r7nHsBo+LxlP4g+AMfyfGP4eEXrA5o/8"
-        crossorigin="anonymous"
+       
     ></script>
 </body>
 </html>
